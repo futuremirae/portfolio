9e09db923fd1601d1f26d3a4a4931758164e460f
futuremirae <kml8486@naver.com>
2024-06-19 14:03:26 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height; // 높이 측정
 
 document.addEventListener("scroll", () => {
-  //   console.log(window.scrollY);
   if (window.scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
   }
+});
+
+// Home section을 아래로 스크롤시 투명하게 처리
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+
+document.addEventListener("scroll", () => {
+  // console.log(object)
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
