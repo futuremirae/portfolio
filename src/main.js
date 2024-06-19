5ca8cb9b2db5e195d35d4c197993625435cf54d1
futuremirae <kml8486@naver.com>
2024-06-19 14:14:19 +0900

    JS-arrow up

--- src/main.js
+++ src/main.js
@@ -16,6 +16,17 @@ const home = document.querySelector(".home__container");
 const homeHeight = home.offsetHeight;
 
 document.addEventListener("scroll", () => {
-  // console.log(object)
   home.style.opacity = 1 - window.scrollY / homeHeight;
+  // opacity 를 정밀하게 조절해야하므로 ~ 직접적으로 스타일 적용
+});
+
+//Arrow up 버튼을 아래로 스크롤시 투명하게 처리함
+const arrow = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= homeHeight / 2) {
+    arrow.style.opacity = 1;
+  } else {
+    arrow.style.opacity = 0;
+  }
 });
