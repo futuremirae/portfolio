25528ea2a6eb6f56b0221d291b64b3a1a8f03dfd
futuremirae <kml8486@naver.com>
2024-06-23 17:18:42 +0900

    Js-active_menu

--- src/active_menu.js
+++ src/active_menu.js
@@ -18,8 +18,12 @@ const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -33,16 +37,24 @@ function observerCallback(entries) {
       entry.isIntersecting &&
       entry.intersectionRatio >= 0.99;
   });
-  console.log(visibleSections);
-  console.log("무조건 라스트 섹션!!", selectLastOne);
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
-  console.log(sectionIds[navIndex]);
+
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
